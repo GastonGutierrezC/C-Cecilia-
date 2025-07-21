326efb4f1c81255b7d5156304d46ce486c30d51d
gaston <gaston.gutierrez.9964359@usalesiana.edu.bo>
2025-07-21 09:56:31 -0400

    update provider response

--- Core/DTOs/ResponseDTOs/Provider/ProciderResponse.cs
+++ Core/DTOs/ResponseDTOs/Provider/ProciderResponse.cs
@@ -2,7 +2,7 @@ namespace Core.DTOs.ResponseDTOs;
 
 public class ProviderResponse
 {
-    public required int ProviderId { get; set; }
+    public required int Id { get; set; }
     public required string Name { get; set; }
     public required double ObjetiveMount { get; set; }
 

82332069a880e6beb56bf8df33c96c1f1e5a89b7
AngheloZambrana <anghelozambrana2005@gmail.com>
2025-07-28 15:02:54 -0400

    feat/UsernameHomepage

--- frontend/src/app/page/home/home.component.ts
+++ frontend/src/app/page/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {RouterLink} from '@angular/router';
+import { UserService } from '../../service/user-service';
 
 @Component({
   selector: 'app-home',
@@ -14,5 +15,11 @@ import {RouterLink} from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private userService: UserService) {
+    if(!this.userService.currentUserId()) {
+      this.userService.getTokenData();
+    }
+  }
 
+  userName = computed(() => this.userService.currentUserId()?.Username || null)
 }
